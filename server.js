0bd76503d92b55e5ae5cc5d8d891fbae03111f22
yomisys <yomisys@gmail.com>
2026-03-01 07:28:28 +0100

    Fix Express options wildcard error

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
-app.options('*', cors());
+
 
 app.use(morgan('dev'));
 app.use(express.json({ limit: '10mb' }));
